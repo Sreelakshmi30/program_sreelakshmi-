a4c59635c8282160b0ed1713c2f9df97f29400d5
Sreelakshmi30 <76092206+Sreelakshmi30@users.noreply.github.com>
2021-02-09 19:08:57 +0530

    Add files via upload

---
+++ co1pg14.py
@@ -0,0 +1,8 @@
+#accept integer n.compute n+nn+nnn#
+
+a = int(input("Input an integer : "))
+n1 = int( "%s" % a )
+n2 = int( "%s%s" % (a,a) )
+n3 = int( "%s%s%s" % (a,a,a) )
+print(n1,n2,n3)
+print (n1+n2+n3)
