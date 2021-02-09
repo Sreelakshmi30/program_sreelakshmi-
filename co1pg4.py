71675a3cfa22a0dedcefa9b6531377b3e304d8b6
Sreelakshmi30 <76092206+Sreelakshmi30@users.noreply.github.com>
2021-02-09 19:04:51 +0530

    Add files via upload

---
+++ co1pg4.py
@@ -0,0 +1,15 @@
+#count the occurence of each word in a line of text#
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print( word_count('the quick brown fox jumps over the lazy dog.'))
