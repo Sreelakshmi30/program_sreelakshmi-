f6defc95a7d2312e29b52d9a580b1a8c89e55bac
Sreelakshmi30 <76092206+Sreelakshmi30@users.noreply.github.com>
2021-02-09 19:09:23 +0530

    Add files via upload

---
+++ co1pg15.py
@@ -0,0 +1,6 @@
+#print all colors from color list1 not contained in color list2#
+
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+
+print(color_list_1.difference(color_list_2))
