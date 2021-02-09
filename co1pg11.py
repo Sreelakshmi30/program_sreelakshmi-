387e3fbb28f8397e56c3f86b1aec14829291f681
Sreelakshmi30 <76092206+Sreelakshmi30@users.noreply.github.com>
2021-02-09 19:07:21 +0530

    Add files via upload

---
+++ co1pg11.py
@@ -0,0 +1,13 @@
+#find biggest of 3 no.s#
+
+num1=int(input("Enter 1st no"))
+num2=int(input("Enter 2nd no"))
+num3=int(input("Enter 3rd no"))
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
