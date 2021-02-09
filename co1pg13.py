2c580f022d6afd6149b720fea1a6d07b252061c6
Sreelakshmi30 <76092206+Sreelakshmi30@users.noreply.github.com>
2021-02-09 19:08:38 +0530

    Add files via upload

---
+++ co1pg13.py
@@ -0,0 +1,4 @@
+#create list of colours from comma separated colour names.display 1st and last color#
+
+color_list = ["Red","Green","White" ,"Black"]
+print( "%s %s"%(color_list[0],color_list[-1]))
